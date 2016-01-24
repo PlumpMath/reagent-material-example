47aa67db043b696fb0fd367bfe8a42e207437c53
Jason Jackson <jasonjckn@gmail.com>
2016-01-24 14:18:06 -0800

    upgraded to cider-nrepl 0.11.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject admin-v2b "0.1.0-SNAPSHOT"
-  :min-lein-version "2.5"
+  :min-lein-version "2.5.3"
   :plugins [[lein-figwheel "0.5.0-4"]]
 
   :dependencies [[org.clojure/clojure "1.7.0"]
@@ -12,22 +12,18 @@
                                     "figwheel_server.log"]
 
 
-  :figwheel {
+  :figwheel {:http-server-root "public" ;; this will be in resources/
+             :css-dirs ["resources/public/css"]
+             :server-port 3449
+             :server-ip "0.0.0.0"
+             :websocket-host :js-client-host
 
-             :http-server-root "public" ;; this will be in resources/
-              :css-dirs ["resources/public/css"]
-              :server-port 3450
-              :server-ip "0.0.0.0"
-              :websocket-host :js-client-host
-              :nrepl-port 7888
-              :nrepl-middleware ["cider.nrepl/cider-middleware"
-                                 ;;"refactor-nrepl.middleware/wrap-refactor"
-                                 "cemerick.piggieback/wrap-cljs-repl"]
+             :nrepl-port 7888
+             :nrepl-middleware ["cider.nrepl/cider-middleware"
+                                ;;"refactor-nrepl.middleware/wrap-refactor"
+                                "cemerick.piggieback/wrap-cljs-repl"]
 
-              :open-file-command "emacsclient"
-
-
-             }
+             :open-file-command "emacsclient"}
 
   :cljsbuild
   {:builds [{:id "dev"
@@ -40,14 +36,13 @@
                         :optimizations :none}}]}
 
   :profiles
-  {:repl {:plugins [[cider/cider-nrepl "0.10.1"]]}
+  {:repl {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]]}
    :dev {:repl-options { ;;:init-ns admin-v2b.core
                         :nrepl-middleware
                         [cemerick.piggieback/wrap-cljs-repl]}
          :dependencies [[com.cemerick/piggieback "0.2.1"]
                         [figwheel-sidecar "0.5.0-SNAPSHOT"]
-                        [cider/cider-nrepl "0.10.1"]
-                        ]
+                        [cider/cider-nrepl "0.11.0-SNAPSHOT"]]
 
          }
 
